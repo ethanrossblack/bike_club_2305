701614c4cfab98de8ee17802f23598fb93274162
Ethan Black <ethanrossblack@gmail.com>
2023-06-05 09:57:55 -0600

    feat: add #log_ride method and refactor @rides initialize method

--- lib/biker.rb
+++ lib/biker.rb
@@ -4,13 +4,17 @@ class Biker
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
-    @rides = {}
+    @rides = Hash.new { |hash, key| hash[key] = [] }
     @acceptable_terrain = []
   end
 
   def learn_terrain!(terrain)
     return nil unless terrain.is_a?(Symbol)
-    
+
     @acceptable_terrain << terrain
+  end
+
+  def log_ride(ride, time)
+    @rides[ride] << time
   end
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -5,6 +5,9 @@ require "./lib/biker"
 describe Biker do
   before :each do
     @biker = Biker.new("Kenny", 30)
+
+    @ride1 = ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   describe "#initialize" do
@@ -19,7 +22,7 @@ describe Biker do
 
     it "starts with no rides" do
       expect(@biker.rides).to eq({})
-
+      expect(@biker.rides).to be_a Hash
     end
 
     it "starts with no accpetable terrain" do
@@ -45,4 +48,37 @@ describe Biker do
       expect(@biker.acceptable_terrain).to eq [:gravel]
     end
   end
+
+  describe "#log_ride" do
+    it "can log a ride" do
+      @biker.log_ride(@ride1, 92.5)
+
+      expect(@biker.rides).to eq({@ride1 => [92.5]})
+
+      @biker.log_ride(@ride2, 60.9)
+      
+      expect(@biker.rides).to eq({@ride1 => [92.5], @ride2 => [60.9]})
+    end
+    
+    it "can keep track of all previous times for each ride" do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides).to eq({@ride1 => [92.5, 91.1], @ride2 => [60.9, 61.6]})
+    end
+  end
 end
+
+
+# 1. Create a Biker with attributes and a way to read that data
+
+# 2. A Biker has a list of acceptable terrain and can learn new terrain
+
+# 3/ Bikers can log a ride with a time. The Biker can keep track of all of its previous rides and times for those rides.
+
+# 4. A Biker will not log a ride if the ride's terrain does not match their acceptable terrain. They also won't log a ride if the ride's total distance is greater than the Biker's max distance.
+
+# 5. A Biker can report its personal record for a specific ride. This is the lowest time recorded for a ride. This method will return false if the Biker hasn't completed the ride
+
