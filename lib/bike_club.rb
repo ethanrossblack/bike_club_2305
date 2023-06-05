78fe9261710350ae206873eb674881f18df1dccc
Ethan Black <ethanrossblack@gmail.com>
2023-06-05 11:13:23 -0600

    feat: add add_biker method and tests

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,6 +1,12 @@
 class BikeClub
-  attr_reader :name
+  attr_reader :name, :bikers
+
   def initialize(name)
     @name = name
+    @bikers = []
+  end
+
+  def add_biker(biker)
+    @bikers << biker if biker.is_a?(Biker)
   end
 end
--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -19,6 +19,34 @@ describe BikeClub do
       expect(@bike_club.name).to eq "2305 Bike Club"
     end
 
+    it "starts with no bikers" do
+      expect(@bike_club.bikers).to eq []
+    end
+
   end
+
+  describe "#add_biker" do
+    
+    before :each do
+      @ethan = Biker.new("Ethan", 100)
+      @zahava = Biker.new("Zahava", 101)
+    end
+
+    it "can add a biker" do
+      @bike_club.add_biker(@ethan)
+      @bike_club.add_biker(@zahava)
+
+      expect(@bike_club.bikers).to eq [@ethan, @zahava]
+    end
+
+    it "can only add Biker objects" do
+      not_a_biker = "I am a string, not a biker"
+
+      @bike_club.add_biker(not_a_biker)
+
+      expect(@bike_club.bikers).to eq []
+    end
   
+  end
+
 end
